d04186b06b5001ea2c5d05e7072ae5080f7823ca
Oluwaferanmi Akinyele <104370294+Oluwaferanmihh@users.noreply.github.com>
2023-08-24 15:45:43 +0100

    Add files via upload

---
+++ practice jav.js
@@ -0,0 +1,30 @@
+
+   { window.alert("You gonna see some magic!!");}
+
+    function magic(){
+        document.getElementById('hide').style.display="none"
+        document.getElementById('magic').style.marginLeft = "300px"
+        document.getElementById('magic').style.display="block"
+
+        }
+    function magic2 (){
+        document.getElementById('magic').style.display="none"
+        document.getElementById('magic2').style.marginTop="400px"
+        document.getElementById('magic2').style.display="block"
+    }    
+    function magic3 (){
+        document.getElementById('magic2').style.display="none"
+        document.getElementById('magic3').style.marginTop="400px"
+        document.getElementById('magic3').style.display="block"
+    }    
+    function magic4 (){
+        document.getElementById('magic3').style.display="none"
+        document.getElementById('magic4').style.marginTop="300px"
+        document.getElementById('magic4').style.marginLeft="150px"
+        document.getElementById('magic4').style.display="block"
+    }    
+    function magic5 (){
+        document.getElementById('magic5').style.display="block"
+        document.getElementById('magic4').style.display="none"
+    
+    }   
